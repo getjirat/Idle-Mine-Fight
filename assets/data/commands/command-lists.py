b485530e6fc0efa8ac207499e5a10bc0da3073bb
getjirat <69470152+getjirat@users.noreply.github.com>
2021-06-10 14:45:12 +0700

    Delete command-lists.py

--- assets/data/commands/command-lists.py
+++
@@ -1,7 +0,0 @@
-class game():
-  class workspace():
-    class gameobjectstorage():
-      def addtoworkspace(object_name,x,y):
-        addsprite = ""
-        addsprite = pygame.image.load("assets/game/workspace/gameobjectstorage/",object_name,".png")
-        screen.blit(addsprite,(x,y))
